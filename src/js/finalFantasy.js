63e5280bfa053e6e03fa0e5629412fd3eb77e33e
Anderson Viana <andersonkallian@yahoo.com.br>
2022-07-31 02:26:21 -0300

    first commit

---
+++ src/js/finalFantasy.js
@@ -0,0 +1,53 @@
+/*
+Objetivo 1 - ao clicar na seta de avançar temos que mostrar
+o proximo cartão da lista:
+
+    - passo 1: pegar oelemento HTML da seta avançar
+    - passo 2: identificar o clique o usuário na seta avançar
+    - passo 3: fazer aparecer o próximo cartao da lista
+    - passo 4: buscar o cartão selecionado e escondê-lo
+
+Objetivo 2 - fazer a mesma coisa pra seta retornar
+*/
+
+const btnAvancar = document.getElementById("btn-avancar")
+const btnVoltar = document.getElementById("btn-voltar")
+
+const cartoes = document.querySelectorAll(".cartao")
+let cartaoAtual = 0
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".selecionado")
+    cartaoSelecionado.classList.remove("selecionado")
+
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado")
+}
+
+
+
+btnAvancar.addEventListener("click", function (){
+
+    if(cartaoAtual === cartoes.length -1) return
+
+    esconderCartaoSelecionado()
+    
+    cartaoAtual++
+    mostrarCartao(cartaoAtual)
+})
+
+
+
+
+btnVoltar.addEventListener("click", function (){
+
+    if(cartaoAtual === 0) return
+
+    esconderCartaoSelecionado()
+
+    cartaoAtual--
+    mostrarCartao(cartaoAtual)
+    
+})
